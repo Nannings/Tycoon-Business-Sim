c96c3e9a7c24128ca4e147269ad7e1811312c49b
mrnannings@gmail.com <mrnannings@gmail.com>
2018-11-07 22:29:43 +0100

    6;

--- Assets/Scripts/store.cs
+++ Assets/Scripts/store.cs
@@ -5,12 +5,17 @@ using UnityEngine.UI;
 
 public class store : MonoBehaviour
 {
+    float CurrentBalance;
+    float BaseStoreCost;
+
     int StoreCount;
     public Text StoreCountText;
 
     private void Start()
     {
         StoreCount = 1;
+        CurrentBalance = 2.00f;
+        BaseStoreCost = 1.50f;
     }
 
     private void Update()
@@ -20,7 +25,10 @@ public class store : MonoBehaviour
 
     public void BuyStoreOnClick()
     {
+        if (BaseStoreCost > CurrentBalance)
+            return;
         StoreCount += 1;
         StoreCountText.text = StoreCount.ToString();
+        CurrentBalance = CurrentBalance - BaseStoreCost;
     }
 }
