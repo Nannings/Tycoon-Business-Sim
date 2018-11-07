87d9802c2308e86614e921d8dc318e037114e211
mrnannings@gmail.com <mrnannings@gmail.com>
2018-11-07 22:16:21 +0100

    4;

---
+++ Assets/Scripts/store.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class store : MonoBehaviour
+{
+    int StoreCount;
+
+    private void Start()
+    {
+        StoreCount = 1;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void BuyStoreOnClick()
+    {
+        StoreCount += 1;
+    }
+}
