bd8d2084e59a23c95760211adff916d0544f68c4
mrnannings@gmail.com <mrnannings@gmail.com>
2018-11-08 19:35:08 +0100

    8;

--- Assets/Scripts/store.cs
+++ Assets/Scripts/store.cs
@@ -7,22 +7,39 @@ public class store : MonoBehaviour
 {
     float CurrentBalance;
     float BaseStoreCost;
+    float BaseStoreProfit;
 
     int StoreCount;
     public Text StoreCountText;
     public Text CurrentBalanceText;
+
+    float StoreTimer = 4f;
+    float CurrentTimer = 0;
+    bool StartTimer;
 
     private void Start()
     {
         StoreCount = 1;
         CurrentBalance = 2.00f;
         BaseStoreCost = 1.50f;
+        BaseStoreProfit = .50f;
         CurrentBalanceText.text = CurrentBalance.ToString("C2");
+        StartTimer = false;
     }
 
     private void Update()
     {
-        
+        if(StartTimer)
+        {
+            CurrentTimer += Time.deltaTime;
+            if(CurrentTimer > StoreTimer)
+            {
+                StartTimer = false;
+                CurrentTimer = 0;
+                CurrentBalance += BaseStoreProfit * StoreCount;
+                CurrentBalanceText.text = CurrentBalance.ToString("C2");
+            }
+        }
     }
 
     public void BuyStoreOnClick()
@@ -33,5 +50,12 @@ public class store : MonoBehaviour
         StoreCountText.text = StoreCount.ToString();
         CurrentBalance = CurrentBalance - BaseStoreCost;
         CurrentBalanceText.text = CurrentBalance.ToString("C2");
+    }
+
+    public void StoreOnClick()
+    {
+        if (!StartTimer)
+            StartTimer = true;
+
     }
 }
