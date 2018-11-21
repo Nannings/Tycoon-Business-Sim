054873ad776ce146af086e5c190868800cf3839b
Nannings <mrnannings@gmail.com>
2018-11-21 20:35:06 +0100

    5-10;

--- Assets/Scripts/LoadGameData.cs
+++ Assets/Scripts/LoadGameData.cs
@@ -28,63 +28,83 @@ public class LoadGameData : MonoBehaviour
 
         xmlDoc.LoadXml(GameData.text);
 
+        LoadGameManagerData(xmlDoc);
+
+        LoadStores(xmlDoc);
+    }
+
+    private static void LoadGameManagerData(XmlDocument xmlDoc)
+    {
         float StartingBalanceNode = float.Parse(xmlDoc.GetElementsByTagName("StartingBalance")[0].InnerText);
         gamemanager.instance.AddToBalance(StartingBalanceNode);
 
         string CompanyName = xmlDoc.GetElementsByTagName("CompanyName")[0].InnerText;
         gamemanager.instance.CompanyName = CompanyName;
+    }
 
+    private void LoadStores(XmlDocument xmlDoc)
+    {
         XmlNodeList StoreList = xmlDoc.GetElementsByTagName("store");
 
         foreach (XmlNode StoreInfo in StoreList)
         {
-            GameObject NewStore = (GameObject)Instantiate(StorePrefab);
+            LoadStoreNodes(StoreInfo);
+        }
+    }
 
-            store storeobj = NewStore.GetComponent<store>();
+    private void LoadStoreNodes(XmlNode StoreInfo)
+    {
+        GameObject NewStore = (GameObject)Instantiate(StorePrefab);
 
-            XmlNodeList StoreNodes = StoreInfo.ChildNodes;
-            foreach (XmlNode StoreNode in StoreNodes)
-            {
-                if (StoreNode.Name == "name")
-                {
-                    Text StoreText = NewStore.transform.Find("StoreNameText").GetComponent<Text>();
-                    StoreText.text = StoreNode.InnerText;
-                }
-                if (StoreNode.Name == "image")
-                {
-                    Sprite newSprite = Resources.Load<Sprite>(StoreNode.InnerText);
-                    Image StoreImage = NewStore.transform.Find("ImageButtonClick").GetComponent<Image>();
-                    StoreImage.sprite = newSprite;
-                }
-                if (StoreNode.Name == "BaseStoreCost")
-                {
-                    storeobj.BaseStoreCost = float.Parse(StoreNode.InnerText);
-                }
-                if (StoreNode.Name == "BaseStoreProfit")
-                {
-                    storeobj.BaseStoreProfit = float.Parse(StoreNode.InnerText);
-                }
-                if (StoreNode.Name == "StoreTimer")
-                {
-                    storeobj.StoreTimer = float.Parse(StoreNode.InnerText);
-                }
+        store storeobj = NewStore.GetComponent<store>();
 
-                if (StoreNode.Name == "StoreMultiplier")
-                {
-                    storeobj.StoreMultiplier = float.Parse(StoreNode.InnerText);
-                }
-                if (StoreNode.Name == "StoreTimerDivision")
-                {
-                    storeobj.StoreTimerDivision = int.Parse(StoreNode.InnerText);
-                }
-                if (StoreNode.Name == "StoreCount")
-                {
-                    storeobj.StoreCount = int.Parse(StoreNode.InnerText);
-                }
+        XmlNodeList StoreNodes = StoreInfo.ChildNodes;
+        foreach (XmlNode StoreNode in StoreNodes)
+        {
+            SetStoreObj(NewStore, storeobj, StoreNode);
+        }
 
-                storeobj.SetNextStoreCost(storeobj.BaseStoreCost);
-                NewStore.transform.SetParent(StorePanel.transform);
-            }
+        storeobj.SetNextStoreCost(storeobj.BaseStoreCost);
+        NewStore.transform.SetParent(StorePanel.transform);
+    }
+
+    private static void SetStoreObj(GameObject NewStore, store storeobj, XmlNode StoreNode)
+    {
+        if (StoreNode.Name == "name")
+        {
+            Text StoreText = NewStore.transform.Find("StoreNameText").GetComponent<Text>();
+            StoreText.text = StoreNode.InnerText;
+        }
+        if (StoreNode.Name == "image")
+        {
+            Sprite newSprite = Resources.Load<Sprite>(StoreNode.InnerText);
+            Image StoreImage = NewStore.transform.Find("ImageButtonClick").GetComponent<Image>();
+            StoreImage.sprite = newSprite;
+        }
+        if (StoreNode.Name == "BaseStoreCost")
+        {
+            storeobj.BaseStoreCost = float.Parse(StoreNode.InnerText);
+        }
+        if (StoreNode.Name == "BaseStoreProfit")
+        {
+            storeobj.BaseStoreProfit = float.Parse(StoreNode.InnerText);
+        }
+        if (StoreNode.Name == "StoreTimer")
+        {
+            storeobj.StoreTimer = float.Parse(StoreNode.InnerText);
+        }
+
+        if (StoreNode.Name == "StoreMultiplier")
+        {
+            storeobj.StoreMultiplier = float.Parse(StoreNode.InnerText);
+        }
+        if (StoreNode.Name == "StoreTimerDivision")
+        {
+            storeobj.StoreTimerDivision = int.Parse(StoreNode.InnerText);
+        }
+        if (StoreNode.Name == "StoreCount")
+        {
+            storeobj.StoreCount = int.Parse(StoreNode.InnerText);
         }
     }
 }
